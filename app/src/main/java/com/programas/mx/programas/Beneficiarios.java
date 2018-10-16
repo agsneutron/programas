40f5a71259cb2c48045f86590627b813027dc768
ariaocho <ariaocho@gmail.com>
2018-10-15 20:45:24 -0500

    beneficiarios agregados a fkujo, marcadores con descripción

--- app/src/main/java/com/programas/mx/programas/Beneficiarios.java
+++ app/src/main/java/com/programas/mx/programas/Beneficiarios.java
@@ -20,8 +20,9 @@ public class Beneficiarios extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                finish();
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
